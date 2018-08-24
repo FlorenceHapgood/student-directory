3e0013963d44289e669bcdc115ed983ab293c728
Florence Hapgood <flohapgood@icloud.com>
2018-08-24 18:42:10 +0100

    input

--- directory.rb
+++ directory.rb
@@ -1,18 +1,25 @@
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit enter twice"
 
+  students = []
 
+  name = gets.chomp
 
-students = [ {name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: 'Nurse Ratched', cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november} ]
+  while !name.empty?
+
+    students << {name: name, cohort: :november}
+
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp #this is refering to the line before, chomping the puts line from "now we have x students"
+    #because name is the same variable as on our while loop, we can reset our original name that way.
+    
+  end
+  students
+end
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +36,8 @@ def print_footer(students)
   puts "Overall we have #{students.count} great students"
 end
 
+
+students = input_students #which returns an array to the students variable
 print_header
 print(students)
 print_footer(students)
