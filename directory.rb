ec38203fc0176d5a595279e7b2713fdf4b7aa5e0
Florence Hapgood <flohapgood@icloud.com>
2018-08-25 20:43:36 +0100

    Load csv

--- directory.rb
+++ directory.rb
@@ -6,6 +6,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from studetns.csv"
   puts "9. Exit"
 end
 
@@ -28,6 +29,8 @@ def process (selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -93,6 +96,17 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| #very interesting
+    name, cohort = line.chomp.split(",") #creates an array with two values.
+    #Also very interesting, naming two variables at once: this is caleld parallel assignment
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 
 
 interactive_menu
