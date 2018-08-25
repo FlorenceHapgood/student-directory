fd2d1109975559c14cd139df0836d94646f805ce
Florence Hapgood <flohapgood@icloud.com>
2018-08-25 14:57:34 +0100

    Refactoring

--- directory.rb
+++ directory.rb
@@ -1,23 +1,65 @@
+@students = [] #so that if you go for option 2 first, it will at least print
+#an empty array. We don't have to worry about the array resetting itself each
+#time because it's outside the loop
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp) #so interesting that you can split the puts and gets like this
+  end
+end
+
+
+def process (selection)
+  case selection
+    when "1"
+      @students = input_students #interesting use of the array here
+      #I reckon this means you can only chose to input the students once
+      #as the array will reset itself.
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+
+  end
+end
+
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit enter twice"
 
-  students = []
+  #@students = []
 
   name = gets.chomp
 
   while !name.empty?
 
-    students << {name: name, cohort: :november}
+    @students << {name: name, cohort: :november}
 
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
 
     name = gets.chomp #this is refering to the line before, chomping the puts line from "now we have x students"
     #because name is the same variable as on our while loop, we can reset our original name that way.
-    
+
   end
-  students
+
 end
 
 
@@ -26,18 +68,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each {|student|
+def print_students_list
+  @students.each {|student|
 puts "#{student[:name]} #{student[:cohort]} cohort"
   }
 end
 
-def print_footer(students)
-  puts "Overall we have #{students.count} great students"
+def print_footer
+  puts "Overall we have #{@students.count} great students"
 end
 
-
-students = input_students #which returns an array to the students variable
-print_header
-print(students)
-print_footer(students)
+interactive_menu
